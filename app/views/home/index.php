bf980230fcaab0d40a156a04853a525a3d6b2629
Muhammad Ramadhan Firdaus <47314308+badon1933@users.noreply.github.com>
2019-08-22 15:39:40 +0700

    Update index.php
    Penambahan kerangka laman home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,91 @@
-<h1>Homepage</h1>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="text-center">
+                <h1 class="display-4">Fluid jumbotron</h1><br>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nulla laudantium quidem totam error</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <h4>
+                <span>Quick Links </span>
+                <span>
+                    <a href="#">Surah Ar-Rahman</a>
+                </span>
+                <span>
+                    <a href="#">Surah Al-Mulk</a>
+                </span>
+                <span>
+                    <a href="#">Ayatul Kursi</a>
+                </span>
+            </h4>
+            <hr>
+        </div>
+    </div>
+
+    <div class="row justify-content-center mb-3">
+        <span>
+            <h3>Surah List</h3>
+        </span>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <ul>
+                <li>
+                    <a href="#" class="row">
+                        <div class="col-2">1</div>
+                        <div class="col-7">Surah Name</div>
+                        <div class="col-3">Arabic</div>
+                        <div class="col-2"></div>
+                        <div class="col-10">Translation</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-md-4">
+            <ul>
+                <li>
+                    <a href="#" class="row">
+                        <div class="col-2">1</div>
+                        <div class="col-7">Surah Name</div>
+                        <div class="col-3">Arabic</div>
+                        <div class="col-2"></div>
+                        <div class="col-10">Translation</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-md-4">
+            <ul>
+                <li>
+                    <a href="#" class="row">
+                        <div class="col-2">1</div>
+                        <div class="col-7">Surah Name</div>
+                        <div class="col-3">Arabic</div>
+                        <div class="col-2"></div>
+                        <div class="col-10">Translation</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="jumbotron mb-0">
+    <div class="row justify-content-center">
+        <span>
+            <h4>This is Footer</h4>
+        </span>
+    </div>
+</div>
