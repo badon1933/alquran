c8be492d1d04339000f84182ee4fd133fe165534
firdaus <badon1933@gmail.com>
2019-08-30 19:46:10 +0700

    Tambah judul laman

--- app/controllers/Recite.php
+++ app/controllers/Recite.php
@@ -4,7 +4,9 @@ class Recite extends Controller
 {
     public function index($chapter_id)
     {
+        $data['judul'] = 'Recite';
         $data['verse'] = $this->model('Verse_model')->getVerse($chapter_id);
+
         $this->view('templates/header', $data);
         $this->view('recite/index', $data);
         $this->view('templates/footer');
